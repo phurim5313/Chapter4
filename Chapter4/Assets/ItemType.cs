561c2ee89b50dc23b9926b0c83e2b1e8ba07dc7b
DESKTOP-325D3AA\Phurim <phurim_sarahnan@cmu.ac.th>
2020-08-14 16:26:15 +0700

    Chapter4

---
+++ Chapter4/Assets/ItemType.cs
@@ -0,0 +1,8 @@
+﻿public enum ItemType {
+    SPHERE_DROP_ITEM, 
+    CUBE_OBSTACLE, 
+    CAPSULE_OBSTACLE, 
+    CYLINDER_OBSTACLE,
+    head2,
+    head3,
+}
---
+++ Chapter4/Assets/ItemTypeComponent.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class ItemTypeComponent : MonoBehaviour
+{
+    [SerializeField]protected ItemType _itemType;
+    // Start is called before the first frame update
+    public ItemType Type
+    {
+        get
+        {
+            return _itemType;
+        }
+        set
+        {
+            _itemType = value;
+        }
+    }
+    
+}
